89d2187a21ea0ce12449a3fd48ac6e6bb8b521e6
ahzzano <w.santos.gg@gmail.com>
2022-05-19 12:47:16 +0800

    added a task complete command

--- src/commands/Tasks.py
+++ src/commands/Tasks.py
@@ -28,7 +28,18 @@ class Tasks(BaseCommand):
                 args[1] = task_name
                 args.append(temp)
 
-            
+            task = list(filter(lambda a: a.content == args[1], state.tasks))[-1]
+
+            if args[-1] == 'complete':
+                if not task.due:
+                    self.api.delete_task(task.id)
+                    return
+
+                due_date = task.due
+                if due_date.recurring:
+                    print('daily task')
+
+            state.set_tasks(self.api.get_tasks())
 
         except Exception as error:
             print(args)

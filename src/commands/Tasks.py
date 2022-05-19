4cce2b80e2590ad186160b213d013f9e20cd6dcf
ahzzano <w.santos.gg@gmail.com>
2022-05-19 12:32:52 +0800

    tasks now accept task names with spaces

--- src/commands/Tasks.py
+++ src/commands/Tasks.py
@@ -5,18 +5,31 @@ from state.state import State
 class Tasks(BaseCommand):
     def on_command(self, args: list, state: State):
         try:
-            tasks = self.api.get_tasks()
+            task_name = args[1]
 
-            sections = {}
+            if '"' in args[1]:
+                task_name = []
 
-            for t in tasks:
-                if not t.project_id in sections.keys():
-                    sections[t.project_id] = []
-                    
-                    
-                sections[t.project_id].append(t.content)
+                ind = 0
 
-            print(sections)
+                for index, i in enumerate(args[1:]):
+                    task_name.append(i)
+                    ind = index
+                    if i[-1] == '"':
+                        break
+
+                task_name = ' '.join(task_name)
+                task_name = task_name.replace('"', '')
+
+                for i in range(1, len(args) - 1):
+                    args.pop(1)
+
+                temp = args[1]
+                args[1] = task_name
+                args.append(temp)
+
+            
 
         except Exception as error:
+            print(args)
             print(error)
